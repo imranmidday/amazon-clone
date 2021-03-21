ca59475e85188406d2836b4cc26da889353a51da
Imran Midday <imranmidday6666@gmail.com>
2021-03-21 21:15:33 +0530

    create card

--- src/Home.js
+++ src/Home.js
@@ -7,35 +7,119 @@ function Home() {
         <div className="home">
             <img
                 className="home__image"
-                src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/Cnnjpp/OnePlus/Week_53/Year_endsale/Family/D18887026_IN_WLD_BAU_OnePlus_Family_DesktopHero_1500x600._CB412699882_.jpg" 
+                src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonDevices/GW/GW_Desktop_Dotbulb_1500X600._CB658001698_.jpg" 
                 alt="amazon-clone-image"
             />
-            {/* Products id, title, price, rating, image*/}
-            <div className="home__row">
-                <Product
-                    id = "12345"
-                    title = "Zero To One"
-                    price = {499}
-                    rating = {5}
-                    image = "https://images-na.ssl-images-amazon.com/images/I/51z7mZZKRgL._SX317_BO1,204,203,200_.jpg"
-                />
-                <Product
-                    id = "12345"
-                    title = "Zero To One"
-                    price = {499}
-                    rating = {5}
-                    image = "https://images-na.ssl-images-amazon.com/images/I/51z7mZZKRgL._SX317_BO1,204,203,200_.jpg"
-                />
-                <Product
-                    id = "12345"
-                    title = "Zero To One"
-                    price = {499}
-                    rating = {5}
-                    image = "https://images-na.ssl-images-amazon.com/images/I/51z7mZZKRgL._SX317_BO1,204,203,200_.jpg"
-                />
+            <div className="home-main-card">
+                <div className="home-header">
+                    <h2 className="home-headline">Top picks for you</h2>
+                </div>
+                <div className="home-card-body">
+                    <div className="home-section grid-row-1">
+                        <div className="home-section container-0">
+                            <a aria-label="ACs" className="link-redirect" href="/login">
+                                <div className="home-section image-container">
+                                    <img alt="ACs" src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/Desktop_QC_tile_graphic_186x116_1._SY116_CB414480869_.jpg"></img>
+                                </div>
+                                <div className="home-section label">
+                                    <span className="home-label">ACs</span>
+
+                                </div>
+                            </a>
+                        </div>
+                        <div className="home-section container-1">
+                            <a aria-label="ACs" className="link-redirect" href="/login">
+                                <div className="home-section image-container">
+                                    <img alt="ACs" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/AmazonBrands/GW/QC/Home/bedsheet_B06Y5GF9SF_Asins_186x116._SY116_CB409842842_.jpg"></img>
+                                </div>
+                                <div className="home-section label">
+                                    <span className="home-label">Home Products</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div className="home-section grid-row-2">
+                        <div className="home-section container-0">
+                            <a aria-label="ACs" className="link-redirect" href="/login">
+                                <div className="home-section image-container">
+                                    <img alt="ACs" src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Fashion/Gateway/Mens_fashion/2._SY116_CB656503003_.jpg"></img>
+                                </div>
+                                <div className="home-section label">
+                                    <span className="home-label">Footware</span>
+                                </div>
+                            </a>
+                        </div>
+                    
+                        <div className="home-section container-1">
+                            <a aria-label="ACs" className="link-redirect" href="/login">
+                                <div className="home-section image-container">
+                                    <img alt="ACs" src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Fashion/Gateway/Mens_fashion/3._SY116_CB656503003_.jpg"></img>
+                                </div>
+                                <div className="home-section label">
+                                    <span className="home-label">Watches</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
             
-            {/* Productd*/}
+            {/* ---------    */}
+            <div className="home-main-card">
+                <div className="home-header">
+                    <h2 className="home-headline">Top picks for you</h2>
+                </div>
+                <div className="home-card-body">
+                    <div className="home-section grid-row-1">
+                        <div className="home-section container-0">
+                            <a aria-label="ACs" className="link-redirect" href="/login">
+                                <div className="home-section image-container">
+                                    <img alt="ACs" src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/Desktop_QC_tile_graphic_186x116_1._SY116_CB414480869_.jpg"></img>
+                                </div>
+                                <div className="home-section label">
+                                    <span className="home-label">ACs</span>
+
+                                </div>
+                            </a>
+                        </div>
+                        <div className="home-section container-1">
+                            <a aria-label="ACs" className="link-redirect" href="/login">
+                                <div className="home-section image-container">
+                                    <img alt="ACs" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/AmazonBrands/GW/QC/Home/bedsheet_B06Y5GF9SF_Asins_186x116._SY116_CB409842842_.jpg"></img>
+                                </div>
+                                <div className="home-section label">
+                                    <span className="home-label">Home Products</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div className="home-section grid-row-2">
+                        <div className="home-section container-0">
+                            <a aria-label="ACs" className="link-redirect" href="/login">
+                                <div className="home-section image-container">
+                                    <img alt="ACs" src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Fashion/Gateway/Mens_fashion/2._SY116_CB656503003_.jpg"></img>
+                                </div>
+                                <div className="home-section label">
+                                    <span className="home-label">Footware</span>
+                                </div>
+                            </a>
+                        </div>
+                    
+                        <div className="home-section container-1">
+                            <a aria-label="ACs" className="link-redirect" href="/login">
+                                <div className="home-section image-container">
+                                    <img alt="ACs" src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Fashion/Gateway/Mens_fashion/3._SY116_CB656503003_.jpg"></img>
+                                </div>
+                                <div className="home-section label">
+                                    <span className="home-label">Watches</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div> 
         </div>
     )
 }
